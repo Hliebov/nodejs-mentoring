3fdf7f9592a7f6502e2ea57f549498fcdbf9aec8
Oleksandr_Hliebov <Oleksandr_Hliebov@epam.com>
2017-10-13 13:25:00 +0300

    hw3 css bundler

--- utils/streams.js
+++ utils/streams.js
@@ -1,100 +1,136 @@
 (function () {
-const fs = require('fs');
-const through = require('through2');
-const csv = require('csvtojson')
+	const fs = require('fs');
+	const through = require('through2');
+	const csv = require('csvtojson');
+	const async = require('async');
+	const path = require('path');
+	const request = require('request');
 
-const HELP = 'help';
-const ACTION ='action';
-const FILE ='file';
+	const HELP = 'help';
+	const ACTION ='action';
+	const FILE ='file';
+	const PATH ='path';
 
-let argv = require('minimist')(process.argv);
-let keys = Object.keys(argv).slice(1);
-let action = argv[ACTION] || argv[ACTION[0]];
-let filePath = argv[FILE] || argv[FILE[0]];
+	const CSS_PATH = 'https://www.epam.com/etc/clientlibs/foundation/main.min.fc69c13add6eae57cd247a91c7e26a15.css';
 
-if (keys.indexOf(HELP) === 1 || keys.indexOf(HELP[0]) === 1) {
-	printHelpMessage();
-	return;
-}
+	let argv = require('minimist')(process.argv);
+	let keys = Object.keys(argv).slice(1);
+	let action = argv[ACTION] || argv[ACTION[0]];
+	let filePath = argv[FILE] || argv[FILE[0]];
+	let cssPath = argv[PATH] || argv[PATH[0]];
 
-if (keys.length < 1) {
-	printHelpMessage();
-}
-
-switch(action) {
-	case 'io':
-		inputOutput(filePath);
-		break;
-	case 'transform-uppercase':
-		transformToUpperCase();
-		break;
-	case 'transform-csv':
-		transformToJson(filePath);
-		break;
-	case 'transform-csv-file':
-		transformFileToJson(filePath);
-		break;
-	default:
+	if (keys.indexOf(HELP) === 1 || keys.indexOf(HELP[0]) === 1) {
 		printHelpMessage();
-}
-
-function inputOutput(path) {
-	fs.createReadStream(path)
-		.pipe(process.stdout);
-}
-
-function transformFileToJson(path) {
-	let fileName = path.split('.')[0];
-	fs.createReadStream(path)
-		.pipe(through(function (chunk, enc, callback) {
-			csv()
-				.fromString(chunk.toString())
-				.on('json',(json)=>{
-					this.push(JSON.stringify(json));
-				});
-
-			callback()
-		}))
-		.pipe(fs.createWriteStream(`${fileName}.json`))
-		.on('finish', function () {
-			console.log('JSON successfully created!')
-		})
-}
-
-function transformToJson(path) {
-	fs.createReadStream(path)
-		.pipe(through(function (chunk, enc, callback) {
-			csv()
-				.fromString(chunk.toString())
-				.on('json',(json)=>{
-					this.push(JSON.stringify(json));
-				});
-
-			callback()
-		}))
-		.pipe(process.stdout);
-}
-
-function transformToUpperCase() {
-	let stream = through(write, end);
-	function write (buffer, encoding, next) {
-		this.push(buffer.toString().toUpperCase());
-		next();
+		return;
 	}
 
-	function end (done) {
-		done();
+	if (keys.length < 1) {
+		printHelpMessage();
 	}
 
-	process.stdin
-		.pipe(stream)
-		.pipe(process.stdout);
-}
+	switch(action) {
+		case 'io':
+			inputOutput(filePath);
+			break;
+		case 'transform-uppercase':
+			transformToUpperCase();
+			break;
+		case 'transform-csv':
+			transformToJson(filePath);
+			break;
+		case 'transform-csv-file':
+			transformFileToJson(filePath);
+			break;
+		case 'bundle-css':
+			bundleCss(cssPath);
+			break;
+		default:
+			printHelpMessage();
+	}
 
-function printHelpMessage() {
-	console.log('Available commands: --file (-f) --action (-a) --help (-h). ' +
-		'Actions: "io", "transform-uppercase", "transform-csv", "transform-csv-file". ' +
-		'Has to have a least one argument!');
-}
+	function inputOutput(path) {
+		fs.createReadStream(path)
+			.pipe(process.stdout);
+	}
+
+	function transformFileToJson(path) {
+		let fileName = path.split('.')[0];
+		fs.createReadStream(path)
+			.pipe(through(function (chunk, enc, callback) {
+				csv()
+					.fromString(chunk.toString())
+					.on('json',(json)=>{
+						this.push(JSON.stringify(json));
+					});
+
+				callback()
+			}))
+			.pipe(fs.createWriteStream(`${fileName}.json`))
+			.on('finish', function () {
+				console.log('JSON successfully created!')
+			})
+	}
+
+	function transformToJson(path) {
+		fs.createReadStream(path)
+			.pipe(through(function (chunk, enc, callback) {
+				csv()
+					.fromString(chunk.toString())
+					.on('json',(json)=>{
+						this.push(JSON.stringify(json));
+					});
+
+				callback()
+			}))
+			.pipe(process.stdout);
+	}
+
+	function transformToUpperCase() {
+		let stream = through(write, end);
+		function write (buffer, encoding, next) {
+			this.push(buffer.toString().toUpperCase());
+			next();
+		}
+
+		function end (done) {
+			done();
+		}
+
+		process.stdin
+			.pipe(stream)
+			.pipe(process.stdout);
+	}
+
+	function printHelpMessage() {
+		console.log('Available commands: --file (-f) --action (-a) --help (-h). ' +
+			'Actions: "io", "transform-uppercase", "transform-csv", "transform-csv-file", "bundle-css" ' +
+			'Has to have a least one argument!');
+	}
+
+	function bundleCss(directory) {
+		fs.readdir(directory, (err, files) => {
+			if (err) {
+				console.error(err);
+			}
+			files = files.map(file => path.join(directory, file));
+			async.map(files, fs.readFile, (err, results) => {
+				if (err) {
+					console.error(err);
+				}
+				request(CSS_PATH, function (error, response, body) {
+					if (error) {
+						return console.error(error);
+					}
+					results.push(body);
+					fs.writeFile(directory + '/bundle.css', results.join("\n"), (err) => {
+						if (err) {
+							console.error(err);
+						} else {
+						}
+					});
+				});
+			});
+		});
+	}
 
 })();
